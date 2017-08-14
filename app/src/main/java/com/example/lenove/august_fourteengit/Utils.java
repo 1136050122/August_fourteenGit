752944f9fe9bce5ff5324e5a21d64a01bca47dd1
11360500122 <2932905597@qq.com>
2017-08-14 15:33:38 +0800

    关于胡歌霍建华

---
+++ app/src/main/java/com/example/lenove/august_fourteengit/Utils.java
@@ -0,0 +1,16 @@
+package com.example.lenove.august_fourteengit;
+
+import android.util.Log;
+
+/**
+ * Created by Lenove on 2017/8/14.
+ */
+
+public class Utils
+{
+    public void getInstance(){
+
+        Log.e("TAG", "getInstance: ");
+
+    }
+}
